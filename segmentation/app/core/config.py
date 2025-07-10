1700dab42e403f28fec2fb2af94b464555c713cf
nbeygi <nima.beygi@abatplus.de>
2025-07-10 21:25:58 +0200

    Fix CORS, Proxy

--- segmentation/app/core/config.py
+++ segmentation/app/core/config.py
@@ -6,7 +6,7 @@ from pydantic import AnyUrl, BeforeValidator
 def parse_cors(v: Any) -> list[str] | str:
     if isinstance(v, str) and not v.startswith("["):
         return [i.strip() for i in v.split(",")]
-    elif isinstance(v, list | str):
+    elif isinstance(v, (list, str)):
         return v
     raise ValueError(v)
 
